be85ba334133b070ab6838e049ae095d29d444bb
1shChheda <1shvenom786@gmail.com>
2022-11-09 02:50:54 +0530

    nodeJS-commit04-CommandLineArgument-&-Yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -61,7 +61,102 @@
 
     // to start nodemon, we type "nodemon app.js" in the Terminal, instead of "node app.js"
         // it will automatically show stuff in Terminal,(upon saving) & we dont have to write "node app.js" again & again
-        const chalk = require('chalk');
-        console.log(chalk.bgRed.bold(`ERROR`));
-        console.log(chalk.bgGreen.bold(`SUCCESS`));
+        // const chalk = require('chalk');
+        // console.log(chalk.bgRed.bold(`ERROR`));
+        // console.log(chalk.bgGreen.bold(`SUCCESS`));
     // to end nodemon (& get back to terminal commands), press Ctrl+C
+
+//Lecture 6 (S3-02) (Section 4 starts)
+    // Javascript in Browser --> window & document
+    // Javascript in NodeJs --> global & process
+
+    // Getting Input from User via COMMAND LINE ARGUEMENTS
+        // BASICS
+        const chalk = require('chalk');
+        // console.log(chalk.bgRed.bold(`ERROR`));
+        // console.log(chalk.bgGreen.bold(`SUCCESS`));
+        // console.log(process.execPath); // returns a string signifies the absolute path of the node.js executable which started the node.js process.
+        // console.log(process.argv); // gives array of 3 strings. 3rd one is that additional input the User typed
+        // console.log(process.argv[2]); // used in arrays in JS
+
+        /*let args = process.argv;
+            console.log("Total number of arguments are: "+args.length);*/
+        /*let cmd = process.argv[2];
+
+        if (cmd === 'add'){
+            console.log(chalk.blue.bgWhite.bold(`Adding Note!`));
+        } else if (cmd === 'remove'){
+            console.log(chalk.red.bgWhite.bold(`Removing Note!`));
+        }*/
+        /* after typing "node app.js" in terminal, every statement written after a <space> becomes an individual arguement, which we can use beneficially. example:
+        Terminal: node app.js add --title="Harry Porter" 
+        note: arguements don't get Parsed i.e. entire raw arguement is taken */
+
+//Lecture 7 (S3-03)
+        const yargs = require('yargs');
+
+        // console.log(process.argv);
+        // console.log(yargs.argv); // to see what parsing has yargs done
+    // Terminal: node app.js add --title="Harry Porter"
+    // Output (from Yargs): { _: [ 'add' ], title: 'Harry Porter', '$0': 'app.js' }
+
+    // Customize Yargs Verion
+        // node app.js --help // to check what COMMANDS have been assigned so far, & other stuff too
+        // node app.js --version (shows version number)(default 1.0.0)
+        yargs.version('1.9.2'); // changes yargs version number
+    
+    // Create Add, Remove, Read, List Notes
+        yargs.command({
+            command: 'add',
+            describe: 'Add a note', // Set describe to false to create a hidden command. Hidden commands don’t show up in the help output and aren’t available for completion.
+            builder: {
+                title: {
+                    describe: 'Note Title',
+                    demandOption: true, // default 'false'; but 'true' will make it a requirement to write a title arguement in COMMAND Line
+                    type: 'string'
+                },
+                body: {
+                    describe: 'main body of the Note',
+                    type: 'string'
+                }
+            },
+            handler: function (argv){
+                console.log(`Title: `+ argv.title)
+                if(argv.body === '' || argv.body === undefined){ //an additional add-on from my end
+                    console.log('No body data yet');
+                } else{
+                    console.log(`Body: `+ argv.body);
+                }
+                
+            }
+        });
+        yargs.command({
+            command: 'remove',
+            describe: 'Remove a note',
+            handler: function (){
+                console.log(`Removing a Note!`)
+            }
+        });
+        yargs.command({
+            command: 'list',
+            describe: 'List all notes',
+            handler: function (){
+                console.log(`Listing all Notes!`)
+            }
+        });
+        yargs.command({
+            command: 'read',
+            describe: 'Read a note',
+            handler: function (){
+                console.log(`Reading a Note!`)
+            }
+        });
+
+    yargs.parse(); // does the Parsing thing without printing entire argv again (like console.log(yargs.argv) does)
+    // console.log(yargs.argv);
+
+    
+
+        
+        
+
