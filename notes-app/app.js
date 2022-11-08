cad4298dfcbf3516c3b92d14f0906361ee585b7c
1shChheda <1shvenom786@gmail.com>
2022-11-08 21:56:16 +0530

    nodeJS-commit02

--- notes-app/app.js
+++ notes-app/app.js
@@ -23,10 +23,24 @@
 
 //Lecture 3
     // const validator = require('validator');
-    //for CORE node Modules, we type out the module_name
-    //for our files, we provide the specific relative path to the file
-    //for npm modules, we type out the npm_package_name
+    // for CORE node Modules, we type out the module_name
+    // for our files, we provide the specific relative path to the file
+    // for npm modules, we type out the npm_package_name
 
     // console.log(validator.isEmail('vansh@awesome.com')); // checks if the email is valid or not
     // console.log(validator.isURL('https://nodejs.com')); //checks if the URL is valid or not
+
+//Lecture 4
+    // for larger applications, node_modules folder can get reall, really big. So, we can delete it when we want to share the code (or add to a Git Repository)
+    // we can always retrieve it back by using 'npm install' command in terminal
+
+    //Challenge 4
+        const chalk = require('chalk');
+        console.log(chalk.green('Hello World!')); //using chalk@4.1.2
+        console.log(chalk.blue('India') + chalk.yellow(' Is') + chalk.red(' The Best')); // Combine styled and normal strings
+        // Compose multiple styles using the chainable API
+            // order in which styles are written doesn't matter
+            console.log(chalk.yellow.italic('Royale'));
+            console.log(chalk.yellow.bgRed.inverse(`Iron Man`)); //'inverse' just inverses the colours of font & background
+
 
