1aba32b1cd6bd62fa422e729de7f35b0b816988b
1shChheda <1shvenom786@gmail.com>
2022-11-14 17:55:24 +0530

    nodeJS-WebServer-commit02-ServingUp-HTML/CSS/IMG/JS

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,14 @@
+const path = require('path')
+
 const express = require('express')
     // express library exports just a single function 
     // i.e. `express` is a function
+
+console.log(__dirname); // path to the directory the app.js lives in
+// console.log(__filename); // path to the file itself
+console.log(path.join(__dirname, `../public`)); 
+    // add strings as arguments to manipulate the main path
+    // `..` goes up a folder
 
 const app = express()
 
@@ -9,6 +17,8 @@ const app = express()
     // app.com/help   {}
     // app.com/about
 // we have set-up multiple routes (/help & /about)
+
+app.use(express.static(path.join(__dirname, `../public`))); // will define the directory which is to be exposed by the web server
 
 app.get('', (req, res) => { // we use `app.get` here to set up our server to send a response when someone tries to go to a specific route
     // 1st argument: route name (like "/help" or "/about")
@@ -19,21 +29,21 @@ app.get('', (req, res) => { // we use `app.get` here to set up our server to sen
     res.send(`<h1>Weather</h1>`);
 })
 
-app.get(`/help`, (req, res) => {
-    // res.send(`Help Page`);
+// app.get(`/help`, (req, res) => {
+//     // res.send(`Help Page`);
 
-    res.send([{
-        content: `important`,
-        origin: `unknown`,
-        provider: `Vansh`
-    },{
-        since: 1997,
-        helpful: 'yes'
-    }])
-    // here, we get a JSON response
-    // express is gonna detect we've sent an object/array & automatically stringify the JSON & send it to requester
+//     res.send([{
+//         content: `important`,
+//         origin: `unknown`,
+//         provider: `Vansh`
+//     },{
+//         since: 1997,
+//         helpful: 'yes'
+//     }])
+//     // here, we get a JSON response
+//     // express is gonna detect we've sent an object/array & automatically stringify the JSON & send it to requester
     
-})
+// })
 
 // Challenge 01: (send simple text message to the route pages)
     // app.get(`/about`, (req, res) => {
@@ -45,19 +55,19 @@ app.get(`/help`, (req, res) => {
 
 // Challenge 02:
     // HTML response to /about
-    app.get(`/about`, (req, res) => {
-        res.send(`<h1>About Us</h1><p>We are a Weather provinding service </p>`);
-    })
+    // app.get(`/about`, (req, res) => {
+    //     res.send(`<h1>About Us</h1><p>We are a Weather provinding service </p>`);
+    // })
 
-    // JSON response to /weather
-    app.get(`/weather`, (req, res) => {
-        res.send([{
-            location: `Virginia`
-        }, {
-            foreCast: `Partly Cloudy`,
-            temperature: 17
-        }]);
-    })
+    // // JSON response to /weather
+    // app.get(`/weather`, (req, res) => {
+    //     res.send([{
+    //         location: `Virginia`
+    //     }, {
+    //         foreCast: `Partly Cloudy`,
+    //         temperature: 17
+    //     }]);
+    // })
 
 app.listen(3000,() => {
     console.log(`Server is UP on port 3000`);
